ffd9dc1b56a0d3f9cd97375e0049346f5e22c55e
Cam Moore <cammymoore07@gmail.com>
2020-12-05 08:44:04 -1000

    Using SubsManager.

--- app/imports/ui/pages/ListItem.jsx
+++ app/imports/ui/pages/ListItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import { SubsManager } from 'meteor/meteorhacks:subs-manager';
 import { Container, Table, Header, Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Items } from '../../api/item/Item';
 import ItemItem from '../components/ItemItem';
+
+// expireLimit set to 30 minutes because: why not.
+const globalSubs = new SubsManager({ expireIn: 30 });
 
 /** Renders a table containing all of the Item documents. Use <ItemItem> to render each row. */
 class ListItem extends React.Component {
@@ -46,7 +50,7 @@ ListItem.propTypes = {
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Item documents.
-  const subscription = Meteor.subscribe(Items.userPublicationName);
+  const subscription = globalSubs.subscribe(Items.userPublicationName);
   return {
     Items: Items.collection.find({}).fetch(),
     ready: subscription.ready(),
--- app/imports/ui/pages/ListStuff.jsx
+++ app/imports/ui/pages/ListStuff.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
+import { SubsManager } from 'meteor/meteorhacks:subs-manager';
 import { Container, Table, Header, Loader } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Stuffs } from '../../api/stuff/Stuff';
 import StuffItem from '../components/StuffItem';
+
+// expireLimit set to 30 minutes because: why not.
+const globalSubs = new SubsManager({ expireIn: 30 });
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListStuff extends React.Component {
@@ -46,7 +49,7 @@ ListStuff.propTypes = {
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Stuffs.userPublicationName);
+  const subscription = globalSubs.subscribe(Stuffs.userPublicationName);
   return {
     stuffs: Stuffs.collection.find({}).fetch(),
     ready: subscription.ready(),
